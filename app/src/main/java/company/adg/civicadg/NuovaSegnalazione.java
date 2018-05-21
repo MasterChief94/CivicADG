8f6d333263697432b0ccd160a8f594c872b7a86f
MasterChief94 <giacomocuccovillo94@gmail.com>
2018-05-21 15:31:15 +0200

    ok

--- app/src/main/java/company/adg/civicadg/NuovaSegnalazione.java
+++ app/src/main/java/company/adg/civicadg/NuovaSegnalazione.java
@@ -1,11 +1,13 @@
 package company.adg.civicadg;
 
 import android.content.Intent;
+import android.media.Image;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NuovaSegnalazione extends AppCompatActivity {
 
@@ -14,9 +16,11 @@ public class NuovaSegnalazione extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuova_segnalazione);
 
-
-        Button PhotoButton = (Button) findViewById(R.id.ImageView);
+        ImageButton PhotoButton = (ImageButton) findViewById(R.id.imageButton);
         PhotoButton.setOnClickListener(PhotoButtonListener);
+
+        //Button PhotoButton = (Button) findViewById(R.id.PhotoButton);
+        //PhotoButton.setOnClickListener(PhotoButtonListener);
     }
 
     public View.OnClickListener PhotoButtonListener = new View.OnClickListener() {
